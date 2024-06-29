152108ce5a83d9e93d8539075a142044785547aa
Numan <n_bd@hotmail.com>
2024-06-29 16:13:42 +0200

    FirstCommit

--- src/main/java/co/wali/Main.java
+++ src/main/java/co/wali/Main.java
@@ -2,23 +2,9 @@ package co.wali;
 
 public class Main {
     public static void main(String[] args) {
-        JavaFun1 javaFunctions = new JavaFun1();
-//        System.out.println(javaFunctions.myFunction(3,7));
-//        System.out.println(javaFunctions.f(6));
-//        System.out.println(javaFunctions.g());
-//
-//        JavaFun2 javaFun2 = new JavaFun2();
-//        System.out.println(javaFun2.h());
-//        System.out.println(javaFun2.g(5));
-//        System.out.println(javaFun2.k(6,6));
-//
-//        Point p1 = new Point(3,4);
-//        Point p2 = new Point(6,8);
-//        System.out.println("Point Distance: " + p1.distance(p2));
-//        System.out.println(Double.parseDouble(" 003.02"));
-
-        PerimeterAssignmentRunner  perimeterRunner = new PerimeterAssignmentRunner ();
-        perimeterRunner.testPerimeter();
+        PerimeterAssignmentRunner  p = new PerimeterAssignmentRunner ();
+        p.testPerimeterMultipleFiles();
+        p.testFileWithLargestPerimeter();
 
     }
 }
--- src/main/java/co/wali/PerimeterAssignmentRunner.java
+++ src/main/java/co/wali/PerimeterAssignmentRunner.java
@@ -11,20 +11,13 @@ import java.util.Objects;
 public class PerimeterAssignmentRunner  {
 
     public double getPerimeter (Shape s) {
-        // Start with totalPerim = 0
         double totalPerim = 0.0;
-        // Start wth prevPt = the last point
         edu.duke.Point prevPt = s.getLastPoint();
-        // For each point currPt in the shape,
         for (Point currPt : s.getPoints()) {
-            // Find distance from prevPt point to currPt 
             double currDist = prevPt.distance(currPt);
-            // Update totalPerim by currDist
             totalPerim = totalPerim + currDist;
-            // Update prevPt to be currPt
             prevPt = currPt;
         }
-        // totalPerim is the answer
         return totalPerim;
     }
 
